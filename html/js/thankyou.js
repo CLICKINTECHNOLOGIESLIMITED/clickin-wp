5f982b548447ab286e603ec0bd702cdeac49b865
clickinwp <clickinwp@dev.sourcefuse.com>
2014-07-11 09:50:45 -0400

    updated html

---
+++ html/js/thankyou.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+   $(".signupContainer").html('<span class="thankyou">Thank you for signing up!</span>');
+   $("#originalImage").hide();
+   $("#revertImage").show();
+   $(".submit").attr('disable','true');
+});
