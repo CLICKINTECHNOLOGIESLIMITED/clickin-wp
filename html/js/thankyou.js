3b9a0b44283e11f4c06df32cc082d14011f1ad68
root <pawan.sharma@sourcefuse.com>
2014-07-11 19:04:37 +0530

    old html removed

--- html/js/thankyou.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-   $(".signupContainer").html('<span class="thankyou">Thank you for signing up!</span>');
-   $("#originalImage").hide();
-   $("#revertImage").show();
-   $(".submit").attr('disable','true');
-});
