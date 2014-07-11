ed5a840c8bc56a9eb1da06baf93f684f6d13c2db
root <pawan.sharma@sourcefuse.com>
2014-07-11 19:13:40 +0530

    html folder

---
+++ html/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+	/* navigation function start here */
+	
+	$("nav ul li").click(function(){
+		
+		$(this).hide();
+		$(this).prev().show();
+		
+	})
+	
+	/* navigation function start here */
+});
