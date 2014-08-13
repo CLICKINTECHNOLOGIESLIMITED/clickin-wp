e6e63ed186e2ca41406cc9d0c4351b1c7bda4a63
clickinwp <clickinwp@dev.sourcefuse.com>
2014-08-13 05:55:03 -0400

    old code removed

--- html/js/thankyou.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$(".signupContainer,.signupContainer1").html('<span class="thankyou">Thank you for signing up!</span>');
-	$(".yourScoreContainer").html('<p class="thankyou">Thank you for signing up! We will keep you updated. </p>');
-	$("#originalImage").hide();
-	$("#revertImage").show();
-	$(".submit").attr('disable','true');
-});
